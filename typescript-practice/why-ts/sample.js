e725d4eef315b92bb1045ede0f43116981406bcb
mori <chohyuk0725@gmail.com>
2022-05-15 20:00:55 +0900

    first commit

---
+++ typescript-practice/why-ts/sample.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns{number}
+ */
+
+//@ts-check
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// sum(10, "20");
+//오류발생 ts-check의 효과때문에 b는 문자열 판단이라 사용될수없음
